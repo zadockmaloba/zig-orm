23438122452fa7ed0ae809b1abf71fddb0f6558a
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2025-03-02 23:08:46 +0300

    Build this project as a module

--- build.zig
+++ build.zig
@@ -4,8 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zigorm",
+    const lib = b.addModule("zigorm", .{
         .root_source_file = b.path("src/database.zig"),
         .target = target,
         .optimize = optimize,
@@ -18,10 +17,8 @@ pub fn build(b: *std.Build) void {
         //.@"disable-ssl" = true,
     });
     const libpq = postgres.artifact("pq");
-    lib.linkLibC();
     lib.linkLibrary(libpq);
 
-    b.installArtifact(lib);
     b.installArtifact(libpq);
 
     lib.addIncludePath(b.path("zig-out/include/"));
@@ -37,6 +34,8 @@ pub fn build(b: *std.Build) void {
 
     lib_unit_tests.addIncludePath(b.path("zig-out/include/"));
     lib_unit_tests.addIncludePath(b.path("zig-out/include/libpq/"));
+
+    lib_unit_tests.root_module.addImport("zigorm", lib);
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
